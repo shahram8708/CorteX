31d0ca9a9aeb3d51b13dede54394bf0682272fbe
Shah Ram <ram.coding8@gmail.com>
2025-04-23 08:58:19 +0530

    added commit

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     phone = db.Column(db.String(15))
     address = db.Column(db.String(200))
     user_type = db.Column(db.String(50))  
