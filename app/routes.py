b6c7e9d804475cb99c23e63968d5a52a06032e7a
Shah Ram <ram.coding8@gmail.com>
2025-04-24 08:07:24 +0530

    added commit

--- app/routes.py
+++ app/routes.py
@@ -126,20 +126,19 @@ def login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=True)
             session.permanent = True
-            
-            # send_login_email(user.email)
-            
-            
+
+            send_login_email(user.email)
+                        
             dashboard = f"{user.user_type}_dashboard"
             return redirect(url_for(f"main.{dashboard}"))
         flash('Invalid email or password', 'danger')
     return render_template('login.html', form=form)
 
-# def send_login_email(user_email):
-#     msg = Message("Successful Login Notification",
-#                   recipients=[user_email])
-#     msg.body = "Dear user, you have successfully logged into your CorteX account."
-#     mail.send(msg)
+def send_login_email(user_email):
+    msg = Message("Successful Login Notification",
+                  recipients=[user_email])
+    msg.body = "Dear user, you have successfully logged into your CorteX account."
+    mail.send(msg)
 
 @main.route('/logout')
 @login_required
